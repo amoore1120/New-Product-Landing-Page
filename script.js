725667bb0cb88447df6017da4711db2f0ccc8229
amoore1120 <amoore1120ka@gmail.com>
2023-06-07 14:40:51 -0400

    hallejahallelujah2

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 window.addEventListener('scroll', function() {
     var header = document.querySelector('.header');
     var scrollPosition = window.scrollY;
+    var w = window.innerWidth
 
     if (scrollPosition > 0) {
         header.classList.add('scrolled');
     } else {
         header.classList.remove('scrolled');
     }
+    if(w <=425){
+        header.classList.remove('scrolled');
+    }
+    if(w <=425){
+        header.classList.remove('scrolled');
+        header.setAttribute('style', 'position: relative;')
+    }
+
+
 });
